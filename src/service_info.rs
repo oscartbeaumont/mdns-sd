45515a98e9e408c102871abaa5a9bff3bee0cbe8
Oscar Beaumont <oscar@otbeaumont.me>
2023-02-27 14:49:51 +0800

    Make `insert_ipv4addr` public

--- src/service_info.rs
+++ src/service_info.rs
@@ -215,7 +215,7 @@ impl ServiceInfo {
     }
 
     /// Insert `addr` into service info addresses.
-    pub(crate) fn insert_ipv4addr(&mut self, addr: Ipv4Addr) {
+    pub fn insert_ipv4addr(&mut self, addr: Ipv4Addr) {
         self.addresses.insert(addr);
     }
 
